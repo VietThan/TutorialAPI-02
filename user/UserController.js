8022d0b3f776a148a3d8895198f1f0fc827b186d
VietThanLinux <thanhoangviet@gmail.com>
2021-06-07 22:53:58 -0400

    Finished tutorial, getting used to Postman

--- user/UserController.js
+++ user/UserController.js
@@ -1,37 +1,55 @@
-// UserController.js
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 var User = require('./User');
-// ADD THIS PART
-// CREATES A NEW USER
-router.post('/', function (req, res) {
+
+// Create a new user
+router.post('/', function(req, res){
     User.create({
-            name : req.body.name,
-            email : req.body.email,
-            password : req.body.password
-        }, 
-        function (err, user) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            res.status(200).send(user);
-        });
-});
-// RETURNS ALL THE USERS IN THE DATABASE
-router.get('/', function (req, res) {
-    User.find({}, function (err, users) {
-        if (err) return res.status(500).send("There was a problem finding the users.");
-        res.status(200).send(users);
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password
+    },
+    function (err, user){
+        if (err) return res.status(500).send("There was a problem adding the information to the database");
+        res.status(200).send(user);
     });
 });
 
-router.get('/:id', function (req, res) {
-    User.findById(req.params.id, function (err, user) {
+// Returns all the users in the database
+router.get('/', function(req, res){
+    User.find({}, function(err, users){
+        if (err) return res.status(500).send("There was a problem finding the users.");
+        res.status(200).send(users)
+    });
+});
+
+// Returns a single user in the databse
+router.get('/:id', function(req, res){
+    User.findById(req.params.id, function (err, user){
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
         res.status(200).send(user);
     });
 });
 
+// Delete a single user from database
+router.delete('/:id', function(req, res){
+    User.findByIdAndDelete(req.params.id, function(err, user){
+        if (err) return res.status(500).send("There was a problem deleting the user.");
+        res.status(200).send("User " + user.name + " was deleted.");
+    });
+});
+
+// Updates a single user in database
+router.put('/:id', function(req, res){
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user){
+        if (err) return res.status(500).send("There was a problem updating the user.");
+        res.status(200).send(user);
+    });
+});
+
+// Export router 
 module.exports = router;
